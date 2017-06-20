5a0a4e98be32b47999149876b49384e748fb79ff
mateusm <mateus.mesturini@gmail.com>
2017-06-20 15:26:00 -0300

    initial commit

---
+++ src/rmm_converter/RMM_Converter.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rmm_converter;
+
+/**
+ *
+ * @author mateusmesturini
+ */
+public class RMM_Converter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frame tela= new frame();
+        tela.show();
+    }
+    
+}
